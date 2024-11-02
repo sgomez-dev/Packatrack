88eb6fd505c5deecb3e3daa44baf2f5896740ba1
santiago.gomez1 <santiago.gomez1@alumnos.uneatlantico.es>
2024-11-02 20:01:42 +0100

    Home Page

--- packatrack/src/Components/layout/HeaderNav.js
+++ packatrack/src/Components/layout/HeaderNav.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../../index.css';
 
 export const HeaderNav = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
     return (
         <nav className="header-nav">
             <div className="logo">Control de Paquetes</div>
-            <ul className="nav-links">
-                <li><Link to="/">Inicio</Link></li>
-                <li><Link to="/registro-diario">Registrar Entregas</Link></li>
-                <li><Link to="/historial-registros">Historial de Registros</Link></li>
-                <li><Link to="/estadisticas">Estadísticas</Link></li>
-                <li><Link to="/perfil">Perfil de Usuario</Link></li> {/* Opcional */}
+            <button className="menu-toggle" onClick={toggleMenu}>
+                ☰
+            </button>
+            <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+                <li><Link to="/" className="nav-button">Inicio</Link></li>
+                <li><Link to="/registro-diario" className="nav-button">Registrar Entregas</Link></li>
+                <li><Link to="/historial-registros" className="nav-button">Historial</Link></li>
+                <li><Link to="/estadisticas" className="nav-button">Estadísticas</Link></li>
+                <li><Link to="/perfil" className="nav-button">Perfil</Link></li>
             </ul>
         </nav>
     );
 };
+
